7eb7afe4d188a99995eaa5d71fcae426e257b7f0
GitHub Action <action@github.com>
2021-02-15 14:58:19 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/riteshgoel/breakpointgroups/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.riteshgoel.breakpointgroups.services
+
+import com.github.riteshgoel.breakpointgroups.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
